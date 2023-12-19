0efb94f7e36202cbcb60ce10e8ba0a3926cfc992
Kishan Kumar Seth <kkseth412@gmail.com>
2023-12-19 11:27:37 +0530

    called the playingMovies api

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,13 @@
 export const netflix_Logo = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+
 export const user_Avatar = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+
 export const signin_Background = "https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NzFkNTFjMTM4MGJiZjk4ZjVkOTkxZjc0OTI4YWE2ZCIsInN1YiI6IjY1ODEyMTg4ZDUxOTFmMDhhNGFkZWE1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LE5FqYeiQptOGFD6x5hua7hor4pFnaSGu8QOSpjm3bI'
+    }
+  };
