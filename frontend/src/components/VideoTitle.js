4855e2a5ffb205dc9c6ef473a9de79fe60e416c9
Kishan Kumar Seth <kkseth412@gmail.com>
2023-12-20 22:06:51 +0530

    truncated the overview to limited char

--- frontend/src/components/VideoTitle.js
+++ frontend/src/components/VideoTitle.js
@@ -1,21 +1,33 @@
-import { play_icon } from "../utils/constant";
-
 const VideoTitle = ({ title, overview }) => {
+
+  // making the overview samller and it contains only "maxChars" characters
+  
+  const truncateOverview = (text, maxChars) => {
+    if (text.length <= maxChars) {
+      return text;
+    }
+  
+    // Find the last sentence within the character limit
+    const lastSentence = text.substr(0, maxChars).replace(/\s[^\s]*$/, '');
+  
+    return `${lastSentence.trim()} . . .`;
+  };
+  
+  const truncatedOverview = truncateOverview(overview, 180);
+
   return (
-    <div className="w-screen aspect-video pt-[15%] px-24 absolute text-white bg-gradient-to-r from-black to-black-10 ">
+    <div className="w-screen aspect-video pt-[15%] px-24 absolute text-white bg-gradient-to-r from-gray-900">
       <h1 className="text-6xl font-semibold w-1/2">{title}</h1>
-      <p className="py-6 text-lg w-1/4">{overview}</p>
+      <p className="py-6 text-lg w-1/4">{truncatedOverview}</p>
 
       <div className="">
         <button className="bg-white text-black p-3 px-12 text-lg rounded-lg hover:bg-opacity-80">
-          {/* <img
-            src={play_icon}
-            alt="Play icon"
-            className="inline-block w-6 h-6 mr-2"
-          />{" "} */}
           ▶ Play
         </button>
-        <button className="ml-2 bg-gray-500 text-white p-3 px-12 text-lg bg-opacity-50 rounded-lg hover:bg-opacity-80"> ℹ️ More Info</button>
+        <button className="ml-2 bg-gray-500 text-white p-3 px-12 text-lg bg-opacity-50 rounded-lg hover:bg-opacity-80">
+          {" "}
+          ℹ️ More Info
+        </button>
       </div>
     </div>
   );
