2d461c06c37c403d24e2d2bb39f9697e0f129697
Kishan Kumar Seth <kkseth412@gmail.com>
2023-12-22 10:39:57 +0530

    fixes

--- frontend/src/components/MoviesCard.js
+++ frontend/src/components/MoviesCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { IMG_CDN_URL } from '../utils/constant'
 
 const MoviesCard = ({posterPath}) => {
+  if(!posterPath) return null;
   return (
     <div className='w-36 pr-4'>
         <img src= {IMG_CDN_URL + posterPath} alt="movie_card" />
