f60e0edf399f4ac5ba3f9b37bba7a9c4cf334137
Kishan Kumar Seth <kkseth412@gmail.com>
2023-12-22 15:54:47 +0530

    popular movie scroll

--- frontend/src/hooks/usePopularMovies.js
+++ frontend/src/hooks/usePopularMovies.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constant";
 import {  addPopularMovies } from "../utils/moviesSlice";
 
@@ -7,9 +7,13 @@ const usePopularMovies = () => {
 
   const dispatch = useDispatch();
 
+  const popularMovies = useSelector(
+    (store) => store.movies.topRatedMovies
+  );
+
   const getPopularMovies = async () => {
     const data = await fetch(
-      "https://api.themoviedb.org/3/movie/popular?page=1",
+      "https://api.themoviedb.org/3/movie/popular?language=hi-IN&region=IN&with_original_language=hi",
       API_OPTIONS
     );
 
@@ -19,7 +23,7 @@ const usePopularMovies = () => {
   };
 
   useEffect(() => {
-    getPopularMovies();
+    !popularMovies && getPopularMovies();
   }, []);
 };
 
