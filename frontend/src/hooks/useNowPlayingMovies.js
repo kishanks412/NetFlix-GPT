49b116463a2ef38ce75a3ee7c1d4d2c3d7b01fe9
Kishan Kumar Seth <kkseth412@gmail.com>
2023-12-22 15:54:21 +0530

    now playing movies scroll

--- frontend/src/hooks/useNowPlayingMovies.js
+++ frontend/src/hooks/useNowPlayingMovies.js
@@ -1,25 +1,29 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constant";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const useNowPlayingMovies = () => {
-
   const dispatch = useDispatch();
+
+  const nowPlayingMovies = useSelector(
+    (store) => store.movies.nowPlayingMovies
+  );
 
   const getNowPlayingMovies = async () => {
     const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      "https://api.themoviedb.org/3/movie/now_playing?language=hi-IN&region=IN&with_original_language=hi",
+       
       API_OPTIONS
     );
 
     const json = await data.json();
-    // console.log(json.results);
+    console.log(json.results);
     dispatch(addNowPlayingMovies(json.results));
   };
 
   useEffect(() => {
-    getNowPlayingMovies();
+    !nowPlayingMovies && getNowPlayingMovies();
   }, []);
 };
 
