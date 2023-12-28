cb569754d0de1af8db00192756db4aef10c34f4f
Kishan Kumar Seth <kkseth412@gmail.com>
2023-12-28 12:17:55 +0530

    made the web app responsive

--- frontend/src/components/MoviesCard.js
+++ frontend/src/components/MoviesCard.js
@@ -1,13 +1,35 @@
-import React from 'react'
-import { IMG_CDN_URL } from '../utils/constant'
+import React from "react";
+import { IMG_CDN_URL } from "../utils/constant";
+import { Link, useNavigate } from "react-router-dom";
 
-const MoviesCard = ({posterPath}) => {
-  if(!posterPath) return null;
+const MoviesCard = ({ movie, size }) => {
+  const navigate = useNavigate();
+  const { poster_path, title, id } = movie;
+
+  if (!movie.poster_path) return null;
+
+  const handleMovie = () => {
+    navigate(`/movie/${id}`);
+  };
+  
+  const classNames = size
+  ? '  w-32 md:w-52'
+  : 'w-28 md:w-40'; 
+
   return (
-    <div className='w-28 md:w-40 pr-4'>
-        <img src= {IMG_CDN_URL + posterPath} alt="movie_card" />
+    <div
+      className={classNames}
+      
+      onClick={handleMovie}
+    >
+      <img
+        // className="w-full h-auto rounded-lg cursor-pointer"
+        className="cursor-pointer rounded-lg hover:scale-110 transition duration-500 md:object-cover"
+        src={IMG_CDN_URL + poster_path}
+        alt={title}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default MoviesCard
+export default MoviesCard;
