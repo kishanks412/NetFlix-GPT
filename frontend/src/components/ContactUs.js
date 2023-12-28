e6062896f857ecc9823ce5f526ffc10570d016d9
Kishan Kumar Seth <kkseth412@gmail.com>
2023-12-28 12:10:43 +0530

    footer added

---
+++ frontend/src/components/ContactUs.js
@@ -0,0 +1,96 @@
+
+import { firestore } from '../utils/firebase';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Formik, Form, useField } from 'formik';
+import * as Yup from 'yup';
+
+const MyTextInput = ({ label, ...props }) => {
+ 
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+
+
+const ContactUs = () => {
+  return (
+    <>
+      <h1>Subscribe!</h1>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+        }}
+        validationSchema={Yup.object({
+          firstName: Yup.string()
+            .max(15, 'Must be 15 characters or less')
+            .required('Required'),
+          lastName: Yup.string()
+            .max(20, 'Must be 20 characters or less')
+            .required('Required'),
+          email: Yup.string()
+            .email('Invalid email address')
+            .required('Required'),
+        
+        })}
+        
+        onSubmit={async (values, { setSubmitting }) => {
+          try {
+            // Store data in Firestore
+            const collectionRef = firestore.collection("User");
+           
+            await collectionRef.add(values);
+            // console.log('Data stored successfully!');
+            // Handle success or navigate to another page
+            // alert('Data stored successfully!');
+          } catch (error) {
+            // Handle error
+            // console.log('Data stored unsuccessfully', error);
+            // console.error('Error storing data:', error);
+          } finally {
+            // Set submitting to false
+            setSubmitting(false);
+          }
+        }}
+      >
+        <Form>
+          <MyTextInput
+            label="First Name"
+            name="firstName"
+            type="text"
+            placeholder="Jane"
+          />
+
+          <MyTextInput
+            label="Last Name"
+            name="lastName"
+            type="text"
+            placeholder="Doe"
+          />
+
+          <MyTextInput
+            label="Email Address"
+            name="email"
+            type="email"
+            placeholder="jane@formik.com"
+          />
+
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </>
+  );
+};
+
+export default ContactUs
+
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,75 @@
+import React from "react";
+import ContactUs from "./ContactUs";
+import { Github, Linkedin, Mail, Phone } from "lucide-react";
+
+const Footer = () => {
+  return (
+    <div className=" flex flex-col items-center bg-red-700">
+      {/* <ContactUs /> */}
+
+      <h1 className="text-xl py-2 md:text-2xl font-semibold md:font-bold">
+        Made by Kishan
+      </h1>
+
+      <hr className="w-[90%]  p-0  md:border-2 border-gray-900" />
+      <div className="flex flex-col py-1 md:px-5 items-center md:flex-row justify-between w-full">
+        <div>
+          <h1 className="text-xl ml-[15%] md:ml-[25%] md:font-semibold">
+            Contact Me
+          </h1>
+          <div className="flex pt-2 ">
+            <a
+              href="https://github.com/yourgithub"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-2 md:mx-3 hover:opacity-75 hover:scale-[120%] transition "
+            >
+              <Github className="w-4 h-4 md:w-6 md:h-6" strokeWidth={1.75} />
+            </a>
+            <a
+              href="https://linkedin.com/in/yourlinkedin"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-2 md:mx-3 hover:opacity-75 hover:scale-[120%] transition"
+            >
+              <Linkedin className="w-4 h-4 md:w-6 md:h-6" strokeWidth={1.75} />
+            </a>
+            <a
+              href="tel:+91 7054073183"
+              className="mx-2 md:mx-3 hover:opacity-75 hover:scale-[120%] transition"
+            >
+              <Phone className="w-4 h-4 md:w-6 md:h-6" strokeWidth={1.75} />
+            </a>
+            <a
+              href="mailto:kishankumarseth.ggu@gmail.com"
+              className="mx-2 md:mx-3 hover:opacity-75 hover:scale-[120%] transition"
+            >
+              <Mail className="w-4 h-4 md:w-6 md:h-6" strokeWidth={1.75} />
+            </a>
+          </div>
+        </div>
+        <div className="">
+          <p className="">
+            ©️ 2023 <span className="text-lg font-semibold">movvify.</span>{" "}
+            Movie Recommendation App,India. <br className="  md:hidden" />  All
+            Rights Reserved
+          </p>
+          <p> PRIVACY POLICY TERMS AND CONDITIONS </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
+
+{
+  /* <div>
+  <h1>Contact Me</h1>
+  <div className='flex '>
+  <Github size={32} color="#000000" strokeWidth={1.75} absoluteStrokeWidth className='cursor-pointer hover:scale-[120%] '/>
+  <Linkedin size={32} color="#000000" strokeWidth={1.75} absoluteStrokeWidth className='cursor-pointer hover:scale-[120%] ' />
+  <Phone size={32} color="#000000" strokeWidth={1.75} absoluteStrokeWidth className=' hover:scale-[120%] '  />
+  </div>
+</div> */
+}
